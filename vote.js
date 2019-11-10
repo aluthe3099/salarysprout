1292455a8ef36bc3a40e5c0b416495d312f6a38f
aluthe3099 <angelineluther@gmail.com>
2019-11-10 09:25:38 -0500

    updates to remove most errors

--- vote.js
+++ vote.js
@@ -1,9 +1,16 @@
 "use strict";
 
-const token = "Your algod API token";
+const algod_address = "http://hackathon.algodev.network";
+  const token = "ef920e2e7e002953f4b29a8af720efe8e4ecc75ff102b165e0472834b25832c1";
+  const algod_client = new algosdk.Algod(token, algod_address, "9100");
+  (async () => {
+    console.log(await algod_client.status());
+  })().catch(e => {
+      console.log(e);
+  });
 const server = "http://127.0.0.1";
 const port = 8080;
-var myAddress = "WTPU6CCKP5PUI73JDJYTQEVPQLAKYTCPKHYD6Y6ZM5AOWMYADVUJZO6WVU";
+var myAddress = "WTP  U6CCKP5PUI73JDJYTQEVPQLAKYTCPKHYD6Y6ZM5AOWMYADVUJZO6WVU";//you need to put the address here
 
 var map1 = new Map();
 function updateVotes(key) {
@@ -41,5 +48,20 @@ function vote(){
 
   let algodclient = algosdk.Algod(token, server, port);
   algosdk.sendRawTransaction(signedTxn.blob);
+}
+function whoGetsRaise()
+{
+  var hasMostVotes = "";
+  var highestValue = -1;
+  for (const k of map1.values())
+  {
+    if((next = k.get())>highestValue)
+    {
+      hasMostVotes = map1.get(k);
+      highestValue = next;
+    }
+  }
+  return hasMostVotes;
+  console.log(map1.getKey(hasMostVotes) + " has " + map1.get(highestValue));
 }
  //algod.sendRawTransaction();
